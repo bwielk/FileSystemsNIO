7d37a5ca1a2bd9b46d1ca0c599c60b1e686e3e38
bwielk <bwielk@gmail.com>
2018-04-30 21:15:52 +0200

    Can walk down the file tree

--- src/file_systemNIO/FileSystemsDirectoriesMain.java
+++ src/file_systemNIO/FileSystemsDirectoriesMain.java
@@ -1,5 +1,6 @@
 package file_systemNIO;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -22,6 +23,38 @@ public class FileSystemsDirectoriesMain {
             }
         }catch(IOException | DirectoryIteratorException e){
             e.printStackTrace();
+        }
+
+        String separator = File.separator;
+        System.out.println("SEPARATOR: " + separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println("SEPARATOR: " + separator);
+
+        try{
+            Path tempFile = Files.createTempFile("directoryApp", ".app");
+            System.out.println("Temporary file created: " + tempFile.toAbsolutePath());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        //STORING DIRECTORIES
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for(FileStore store : stores){
+            System.out.println(store + " VOLUME NAME: " + store.name());
+        }
+
+        //ROOT DIRECTORIES
+        Iterable<Path> paths = FileSystems.getDefault().getRootDirectories();
+        for(Path currentPath : paths){
+            System.out.println(currentPath);
+        }
+
+        //WALKING DOWN THE DIRECTORY TREE
+        Path dir2Path = FileSystems.getDefault().getPath("Examples" + File.separator + "dir2");
+        try{
+            Files.walkFileTree(dir2Path, new PrintNames());
+        }catch(IOException e){
+            e.getMessage();
         }
     }
 }
